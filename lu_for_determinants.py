c569d147037732f10fdbcc3cc8e010e00284a1fc
sanaa256 <134206911+sanaa256@users.noreply.github.com>
2025-12-07 11:57:44 +0000

    set up lu for calculating determinants

---
+++ lu_for_determinants.py
@@ -0,0 +1,47 @@
+from lu_factorisation import lu_factorisation
+import numpy as np
+
+from scipy.sparse import diags
+
+def generate_safe_system(n):
+    """
+    Generate a linear system A x = b where A is strictly diagonally dominant,
+    ensuring LU factorization without pivoting will work.
+
+    Parameters:
+        n (int): Size of the system (n x n)
+
+    Returns:
+        A (ndarray): n x n strictly diagonally dominant matrix
+        b (ndarray): RHS vector
+        x_true (ndarray): The true solution vector
+    """
+
+    k = [np.ones(n - 1), -2 * np.ones(n), np.ones(n - 1)]
+    offset = [-1, 0, 1]
+    A = diags(k, offset).toarray()
+
+    # Solution is always all ones
+    x_true = np.ones((n, 1))
+
+    # Compute b = A @ x_true
+    b = A @ x_true
+
+    return A, b, x_true
+
+A_large, b_large, x_large = generate_safe_system(100)
+
+def determinant(A):
+    n = A.shape[0]
+    L, U = lu_factorisation(A)
+
+    det_L = 1.0
+    det_U = 1.0
+
+    for i in range(n):
+        det_L *= L[i, i]
+        det_U *= U[i, i]
+
+    return det_L * det_U
+
+print(determinant(A_large))
